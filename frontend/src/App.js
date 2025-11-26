93f176cd68ff94c93a865148caafabfc548bbaf6
GimaR03 <it23673922@my.sliit.lk>
2025-11-26 22:04:42 +0530

    Update App.js
    UpUpdate App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,17 @@ import ContactMe from './Components/contactme';
 import Gallery from './Components/gallery';
 import Certification from './Components/certification';
 import Achievement from './Components/achivement';
+// Remove the missing imports
+// import BloomHabit from './Components/bloomHabit';
+// import MedicalPortal from './Components/medicalPortal';
+// import AnimalPlant from './Components/animalPlant';
+// import Fitme from './Components/fitme';
+// import Farm from './Components/farm';
 
 function App() {
   return (
     <Router>
-      <div className="App min-h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="App min-h-screen bg-slate-100 dark:bg-slate-900 transition-colors duration-300">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,6 +27,12 @@ function App() {
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/certifications" element={<Certification />} />
           <Route path="/achievements" element={<Achievement />} />
+          {/* Remove the project detail routes until you create the components */}
+          {/* <Route path="/bloomHabit" element={<BloomHabit />} />
+          <Route path="/medicalPortal" element={<MedicalPortal />} />
+          <Route path="/animalPlant" element={<AnimalPlant />} />
+          <Route path="/fitme" element={<Fitme />} />
+          <Route path="/farm" element={<Farm />} /> */}
         </Routes>
       </div>
     </Router>
