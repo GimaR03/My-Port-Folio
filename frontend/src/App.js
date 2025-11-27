46dc49be1fccd510775963001c1f79463dd87c4a
GimaR03 <it23673922@my.sliit.lk>
2025-11-27 19:00:02 +0530

    Update App.js
    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/navbar';
 import Home from './Components/home';
+import About from './Components/about';
 import ViewProject from './Components/viewproject';
 import ContactMe from './Components/contactme';
-import Gallery from './Components/gallery';
 import Certification from './Components/certification';
-import Achievement from './Components/achivement';
-// Remove the missing imports
-// import BloomHabit from './Components/bloomHabit';
-// import MedicalPortal from './Components/medicalPortal';
-// import AnimalPlant from './Components/animalPlant';
-// import Fitme from './Components/fitme';
-// import Farm from './Components/farm';
+import BloomHabit from './Components/bloomHabit';
+import MedicalPortal from './Components/medicalPortal';
+import AnimalPlant from './Components/animalPlant';
+import Fitme from './Components/fitme';
+import Farm from './Components/farm';
+import CakeShop from './Components/cakeShop';
+import VehicleSpire from './Components/vehicleSpire';
 
 function App() {
   return (
@@ -22,17 +22,18 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/projects" element={<ViewProject />} />
           <Route path="/contact" element={<ContactMe />} />
-          <Route path="/gallery" element={<Gallery />} />
           <Route path="/certifications" element={<Certification />} />
-          <Route path="/achievements" element={<Achievement />} />
-          {/* Remove the project detail routes until you create the components */}
-          {/* <Route path="/bloomHabit" element={<BloomHabit />} />
+          {/* Project Detail Routes */}
+          <Route path="/bloomHabit" element={<BloomHabit />} />
           <Route path="/medicalPortal" element={<MedicalPortal />} />
           <Route path="/animalPlant" element={<AnimalPlant />} />
           <Route path="/fitme" element={<Fitme />} />
-          <Route path="/farm" element={<Farm />} /> */}
+          <Route path="/farm" element={<Farm />} />
+          <Route path="/cakeShop" element={<CakeShop />} />
+          <Route path="/vehicleSpire" element={<VehicleSpire />} />
         </Routes>
       </div>
     </Router>
